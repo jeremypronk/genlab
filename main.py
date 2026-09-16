87e1b6a1ab29a96b061c28ff083f89c670fa3bda
Jeremy Pronk <72781694+jeremypronk@users.noreply.github.com>
2026-09-16 15:39:12 +1000

    refactor wip kie

--- main.py
+++ main.py
@@ -55,27 +55,22 @@ class GenAPI:
         queuing = 5
         unknown = 6
     
-    def __init__(self):
-        self._api_server = None
-        self._base_api_url = None
-        self._upload_url = None
-
-
-class KieAIVideoGen(GenAPI):
+class KieAIGen(GenAPI):
     """
+    Base class for kie.ai api
     """
-    _upload_cache = {} # upload cache is shared across all instances of video gen
+    _api_server = "https://api.kie.ai"
+    _base_api_url = f"{self._api_server}/api/v1"
+    _create_task_url = f"{self._base_api_url}/jobs/createTask"
+    _query_task_url = f"{self._base_api_url}/jobs/recordInfo"
+    _upload_url = "https://kieai.redpandaai.co/api/file-stream-upload"
 
+    # upload cache is shared across all instances of kie ai gen subclasses
+    _upload_cache = {} 
+    
     def __init__(self, api_key, output_basepath, task_id=None, path_converter_func=lambda x: x):
-        logging.debug(f"KieAIVideoGen(api_key={api_key}, task_id={task_id})")
+        logging.debug(f"KieAIGen(api_key={api_key}, task_id={task_id})")
         super().__init__()
-
-        # kie.ai
-        self._api_server = "https://api.kie.ai"
-        self._base_api_url = f"{self._api_server}/api/v1"
-        self._create_task_url = f"{self._base_api_url}/jobs/createTask"
-        self._query_task_url = f"{self._base_api_url}/jobs/recordInfo"
-        self._upload_url = "https://kieai.redpandaai.co/api/file-stream-upload"
         
         self._api_key = api_key
         
@@ -89,6 +84,15 @@ class KieAIVideoGen(GenAPI):
             "Content-Type": "application/json"
         }
         self._path_converter_func = path_converter_func
+
+class KieAIVideoGen(KieAIGen):
+    """
+    Base class for kie.ai video gen api
+    """
+
+    def __init__(self, api_key, output_basepath, task_id=None, path_converter_func=lambda x: x):
+        logging.debug(f"KieAIVideoGen(api_key={api_key}, task_id={task_id})")
+        super().__init__(api_key, output_basepath, task_id, path_converter_func))
 
     def __repr__(self):
         return f"KieAIVideoGen({self._task_id}, {self._output_basepath})"
